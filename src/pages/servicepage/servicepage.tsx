c45958c11723f56a9f4e85edb996e68fc53f676b
hamai <mhamail1223@gmail.com>
2023-10-18 12:41:00 +0500

    angry

---
+++ src/pages/servicepage/servicepage.tsx
@@ -0,0 +1,13 @@
+import CompanyForm from '@/components/DataEntry/form/CompanyForm'
+import Layout from '@/components/layout/Layout'
+import React from 'react'
+
+const Servicepage = () => {
+  return (
+    <Layout>
+      <CompanyForm/>
+    </Layout>
+  )
+}
+
+export default Servicepage
